9419551e145ba4186f901c8b09948043c90bf5b1
Harvey Mortimore <77932367+hmortimore69@users.noreply.github.com>
2025-01-29 14:53:43 +0000

    Completed events101

--- index.js
+++ index.js
@@ -2,3 +2,72 @@
 /**
  * Add your functions here...
  */
+
+function targetTextToConsole(event) {
+    console.log(event.target.textContent);
+}
+
+function tttcAttacher() {
+    const button0 = document.getElementById("button0");
+    button0.addEventListener("click", e => targetTextToConsole(e));
+}
+
+function lovelyParaAttacher() {
+    const para = document.getElementById("thisisalovelyparagraph");
+    para.addEventListener("click", lovelyToggle);
+}
+
+function lovelyButtonAttacher() {
+    const button1 = document.getElementById("button1");
+    button1.addEventListener("click", lovelyToggle);
+}
+
+function concatAttacher() {
+    const in1 = document.getElementById("in1");
+    const in2 = document.getElementById("in2");
+
+    function handler() {
+        const out1 = document.getElementById("out1");
+
+        out1.textContent = in1.value + in2.value;
+    }
+
+    in1.addEventListener("change", handler);
+    in2.addEventListener("change", handler);
+}
+
+function snitchAttacher() {
+    const mouseWatcher = document.getElementById("mousewatcher");
+    
+    mouseWatcher.addEventListener("mouseover", snitchUpdater);
+    mouseWatcher.addEventListener("mouseout", snitchUpdater);
+}
+
+function snitchUpdater(e) {
+    const snitch = document.getElementById("snitch");
+    snitch.textContent = e.type === "mouseover" ? "IN" : "OUT";
+}
+
+function reportAttacher() {
+    const mouseReporter = document.getElementById("mousereporter");
+
+    mouseReporter.addEventListener("mousemove", reportUpdater);
+}
+
+function reportUpdater(event) {
+    const report = document.getElementById("report");
+
+    report.textContent = `x: ${event.screenX} y: ${event.screenY}`;
+}
+
+function idValidationAttacher() {
+    const newid = document.getElementById("newid");
+
+    newid.addEventListener("input", function () {
+        if (newid.value.includes(" ")) {
+            newid.classList.add("invalid");
+        } else {
+            newid.classList.remove("invalid");
+        }
+    }); 
+}
