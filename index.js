727a0acb076de196832ea4b9e20b1c9915b97f7e
Harvey Mortimore <77932367+hmortimore69@users.noreply.github.com>
2025-02-05 12:14:19 +0000

    Made comments more maintainable

--- index.js
+++ index.js
@@ -3,25 +3,52 @@
  * Add your functions here...
  */
 
+/*
+    Inputs: event: Event
+    Return: None
+    Description: This function outputs the textContent of the events target to the console.
+*/
 function targetTextToConsole(event) {
     console.log(event.target.textContent);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function outputs the TextContent of #button0 to the console.
+*/
 function tttcAttacher() {
     const button0 = document.querySelector("#button0");
     button0.addEventListener("click", e => targetTextToConsole(e));
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function calls the provided lovelyToggle function when the #thisisalovelyparagraph is clicked.
+*/
 function lovelyParaAttacher() {
     const para = document.querySelector("#thisisalovelyparagraph");
     para.addEventListener("click", lovelyToggle);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function attaches the event "click" that calls the listenerlovelyToggle to button1.
+*/
 function lovelyButtonAttacher() {
     const button1 = document.querySelector("#button1");
     button1.addEventListener("click", lovelyToggle);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function attaches an event listener to the `in1` and `in2` input elements.
+    When `in1` and `in2` change, their content should be joined together and placed inside the `out1` element, 
+    such that if you entered `Darth` and `Vader` the content of `out1` would be `DarthVader`.
+*/
 function concatAttacher() {
     const in1 = document.querySelector(`#in1`);
     const in2 = document.querySelector(`#in2`);
@@ -35,6 +62,12 @@ function concatAttacher() {
     in2.addEventListener("change", handler);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function attaches the event listener "mouseover" and "mouseout" to the 
+    #mousewatcher element that calls the snitchUpdater listener.
+*/
 function snitchAttacher() {
     const mouseWatcher = document.querySelector("#mousewatcher");
     
@@ -42,18 +75,34 @@ function snitchAttacher() {
     mouseWatcher.addEventListener("mouseout", snitchUpdater);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function attaches the event "click" to call the listener "reportUpdater" on button1.
+*/
 function reportAttacher() {
     const mouseReporter = document.querySelector("#mousereporter");
 
     mouseReporter.addEventListener("mousemove", reportUpdater);
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function modifies the textContent of #report to be the coordinates of the mouse.
+*/
 function reportUpdater(event) {
     const report = document.querySelector("#report");
 
     report.textContent = `x: ${event.screenX} y: ${event.screenY}`;
 }
 
+/*
+    Inputs: None
+    Return: None
+    Description: This function attaches the event "input" to the listener function for #newid.
+    The listener function adds the "invalid" class if the input includes a space, and removes it when it doesn't.
+*/
 function idValidationAttacher() {
     const newid = document.querySelector("#newid");
 
